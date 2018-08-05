0644bbf364523ea2c0f1b01eb054d83bcad8db6f
saraivacode <tiagosarai@gmail.com>
2018-08-05 20:45:44 -0300

    Update local_controllers.sh

--- local_controllers.sh
+++ local_controllers.sh
@@ -31,7 +31,12 @@ function calc {
 		do
 			mysql -u root -pwifi -e "select sum(data_rate) from appkpi where id IN (select app_id from vehicle where mac = '"$i"') and class='"C"'" framework 2> /dev/null |tail -1
 		done | paste -s | expand | tr -s ' ' | sed 's/ /+/g' |sed 's/NULL/0/g'| bc)
-	fi
+	else
+		y=0
+		b=0
+		c=0
+	fi	
+
 	#Verifica se existem fluxos cadastrados cuja RSUS em análise é destino (para debitar do saldo)
 	dec=$(mysql -u root -pwifi -e "select sum(bw_value) from redirect where rsu_dest= '"$rsu_calc"' " framework 2> /dev/null | tail -1)
 	#Verifica se esxistem fluxos cadastrados cuja RSUS em análise é origem (para incrementar do saldo)
@@ -46,7 +51,7 @@ function calc {
 	fi					
 	#Calcula o saldo da RSU em análise
 	sd=$(echo $up-$y-$dec+$inc|bc)
-	#echo -e "\n rsu: " $rsu " dec: " $dec " inc: " $inc " sd: " $sd "y: " $y
+	#echo -e "\n rsu: " $rsu_calc " dec: " $dec " inc: " $inc " sd: " $sd "y: " $y
 	#zera arquivo de saldos que será utilizado como referência
 	#Salva saldo calculado em arquivo temporario de controle, para uso nos demais algoritmos
 	#echo $(echo $j | cut -d'-' -f1) $sd >> saldo.txt
@@ -54,6 +59,8 @@ function calc {
 	echo $rsu_calc $sd >> temp.tmp
 	rm saldo.txt
 	mv temp.tmp saldo.txt
+	# echo "saldo"
+	# cat saldo.txt
 }
 
 function limpeza {
@@ -126,14 +133,17 @@ do
 	#para cada rsu no arquivo de saldos
 	for rsu in $(cat saldo.txt | cut -d' ' -f1);
 	do
-		rm -f appc.txt
-		rm -f appb.txt
 		#se o saldo for menor que zero (precisa de acao). Se for a rsu1 ou rsu3 (que só tem a rsu2 como vizinhos)
 		if [[ $(cat saldo.txt | grep $rsu | cut -d' ' -f2) -lt 0 ]] && [[ $rsu != "rsu2" ]]; then
 			#enquanto o saldo for negativo na RSU1 ou RSU3
 			while [[ $(cat saldo.txt | grep $rsu | cut -d' ' -f2) -lt 0 ]]; do
 				#identifica os MACs na RSU1 ou RSU3
 				mac_rsu1=$(hostapd_cli -i $rsu-wlan1 all_sta | grep :)
+
+				#inicializa arquivos de controle das aplicacoes
+				rm -f appc.txt
+				rm -f appb.txt
+
 				#identifica os MACs na rsu1 ou rsu3 e saldo nos respectivos arquivos se já não foi redirecionado/bloqueado
 				for i in $mac_rsu1;
 				do
@@ -290,12 +300,17 @@ do
 				calc 
 			done
 		#se for o caso da RSU2
-		else
+		elif [[ $(cat saldo.txt | grep $rsu | cut -d' ' -f2) -lt 0 ]] && [[ $rsu = "rsu2" ]]; then
 			#enquanto o saldo for negativo na RSU2
 			while [[ $(cat saldo.txt | grep $rsu | cut -d' ' -f2) -lt 0 ]]; do
 				#identifica os MACs na RSU2
 				mac_rsu1=$(hostapd_cli -i $rsu-wlan1 all_sta | grep :)
 				#Se MACS estao associados a aplicasoes B ou C e nao foram redirecionados ou bloqueados, salva nos respectivos arquivos
+
+				#inicializa arquivos de controle das aplicacoes
+				rm -f appc.txt
+				rm -f appb.txt
+				
 				for i in $mac_rsu1;
 				do
 					res_c=$(mysql -u root -pwifi -e "select sum(data_rate) from appkpi where id IN (select app_id from vehicle where mac = '"$i"') and class='"C"'" framework 2> /dev/null |tail -1)
@@ -309,6 +324,11 @@ do
 						then echo $i >> appc.txt;
 					fi
 				 done
+
+				 # echo "appb.txt"
+				 # cat appb.txt
+				 # echo "appc.txt"
+				 # cat appc.txt
 				#Para vizinho rsu1, verifica se aceita aplicação C e existe MAC associado a aplicação C
 				if [[ $(cat saldo.txt | grep rsu1 | cut -d' ' -f2) -ge $appc_bw ]] && [[ $(cat appc.txt | wc -l) -gt 0 ]]; then
 					for i in $(cat appc.txt);
@@ -336,7 +356,7 @@ do
 							mysql -u root -pwifi -e "insert into redirect (mac, rsu_o, rsu_dest, bw_value) values (\"$i\", \"$rsu\", \"rsu1\", $appc_bw)" framework 2> /dev/null
 							#para calcular o saldo na rsu1
 							rsu_calc=rsu1
-							x=$(hostapd_cli -i rsu2-wlan1 all_sta | grep :)
+							x=$(hostapd_cli -i rsu1-wlan1 all_sta | grep :)
 							calc 
 							rsu_calc=$rsu
 							x=$(hostapd_cli -i $rsu-wlan1 all_sta | grep :)
@@ -370,7 +390,7 @@ do
 							mysql -u root -pwifi -e "insert into redirect (mac, rsu_o, rsu_dest, bw_value) values (\"$i\", \"$rsu\", \"rsu3\", $appc_bw)" framework 2> /dev/null
 							#para calcular o saldo na rsu1
 							rsu_calc=rsu3
-							x=$(hostapd_cli -i rsu2-wlan1 all_sta | grep :)
+							x=$(hostapd_cli -i rsu3-wlan1 all_sta | grep :)
 							calc 
 							rsu_calc=$rsu
 							x=$(hostapd_cli -i $rsu-wlan1 all_sta | grep :)
@@ -404,7 +424,7 @@ do
 							mysql -u root -pwifi -e "insert into redirect (mac, rsu_o, rsu_dest, bw_value) values (\"$i\", \"$rsu\", \"rsu1\", $appb_bw)" framework 2> /dev/null
 							#Calcula saldos
 							rsu_calc=rsu1
-							x=$(hostapd_cli -i rsu2-wlan1 all_sta | grep :)
+							x=$(hostapd_cli -i rsu1-wlan1 all_sta | grep :)
 							calc 
 							rsu_calc=$rsu
 							x=$(hostapd_cli -i $rsu-wlan1 all_sta | grep :)
@@ -438,7 +458,7 @@ do
 							mysql -u root -pwifi -e "insert into redirect (mac, rsu_o, rsu_dest, bw_value) values (\"$i\", \"$rsu\", \"rsu3\", $appb_bw)" framework 2> /dev/null
 							#Calcula saldos
 							rsu_calc=rsu3
-							x=$(hostapd_cli -i rsu2-wlan1 all_sta | grep :)
+							x=$(hostapd_cli -i rsu3-wlan1 all_sta | grep :)
 							calc 
 							rsu_calc=$rsu
 							x=$(hostapd_cli -i $rsu-wlan1 all_sta | grep :)
@@ -473,7 +493,7 @@ do
 
 					echo "Aplicacao C em $i estava redirecionada para $rsudst e sera bloqueada."
 					#Bloqueia aplicacao C na fonte
-					ovs-ofctl add-flow $rsu "table=1, priority=2, cookie=0x$(echo $rsu | cut -d'u' -f2)5, in_port=1,dl_src=$i, nw_dest=200.0.10.4, udp,tp_dst=5004 actions=drop" -O Openflow13
+					ovs-ofctl add-flow $rsu "table=1, priority=2, cookie=0x$(echo $rsu | cut -d'u' -f2)5, in_port=1,dl_src=$i, nw_dst=200.0.10.4, udp,tp_dst=5004 actions=drop" -O Openflow13
 					#Apaga redirecionamentos da aplicacao C
 					ovs-ofctl del-flows $rsu cookie=0x1$(echo $rsu | cut -d'u' -f2)/-1,dl_src=$i,nw_dst=200.0.10.4,udp,tp_dst=5004 -O Openflow13
 					ovs-ofctl del-flows $rsudst cookie=0x1$(echo $rsu | cut -d'u' -f2)/-1,dl_dst=$i,udp,tp_src=5004 -O Openflow13
@@ -484,7 +504,7 @@ do
 					mysql -u root -pwifi -e "update redirect set rsu_dest=\"x\" where mac=\"$i\" and bw_value=$appc_bw" framework 2> /dev/null
 					#recalcula saldo no vizinho
 					rsu_calc=$rsudst
-					x=$(hostapd_cli -i rsu2-wlan1 all_sta | grep :)
+					x=$(hostapd_cli -i $rsudst-wlan1 all_sta | grep :)
 					calc
 				#último caso, bloqueia aplicações B
 				elif [[ $(cat appb.txt | wc -l) -gt 0 ]]; then
