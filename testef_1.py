9e0dee5bf775550e37cd9bec9bc0af96fd9c08fd
saraivacode <tiagosarai@gmail.com>
2018-08-14 08:06:52 +0000

    initial positions

--- testef_1.py
+++ testef_1.py
@@ -109,62 +109,7 @@ def topology():
     net.plotGraph(max_x=3000, max_y=3000)
 
     # net.roads(10)
-
-    net.startMobility( time=0 )
-    net.mobility( cars[0], 'start', time=1, position='120,1000,0' )
-    net.mobility( cars[0], 'stop', time=179, position='2101,1000,0' )
-    net.mobility( cars[1], 'start', time=1, position='130,999,0' )
-    net.mobility( cars[1], 'stop', time=179, position='2095,999,0' )
-    net.mobility( cars[2], 'start', time=1, position='135,997,0' )
-    net.mobility( cars[2], 'stop', time=59, position='2097,997,0' )
-    net.mobility( cars[3], 'start', time=1, position='140,1000,0' )
-    net.mobility( cars[3], 'stop', time=59, position='1601,1000,0' )
-    net.mobility( cars[4], 'start', time=1, position='145,999,0' )
-    net.mobility( cars[4], 'stop', time=59, position='1599,999,0' )
-    net.mobility( cars[5], 'start', time=1, position='150,997,0' )
-    net.mobility( cars[5], 'stop', time=179, position='1595,997,0' )
-    net.mobility( cars[6], 'start', time=1, position='155,1000,0' )
-    net.mobility( cars[6], 'stop', time=179, position='1101,1000,0' )
-    net.mobility( cars[7], 'start', time=1, position='160,1000,0' )
-    net.mobility( cars[7], 'stop', time=179, position='1098,1000,0' )
-    net.mobility( cars[8], 'start', time=1, position='165,999,0' )
-    net.mobility( cars[8], 'stop', time=179, position='1095,999,0' )
-    net.stopMobility( time=180 )
-
-    # cars[0].setPosition('1103,1000,0')
-    # cars[1].setPosition('1105,1000,0')
-    # cars[2].setPosition('1095,1000,0')
-    # cars[3].setPosition('1603,1000,0')
-    # cars[4].setPosition('1605,1000,0')
-    # cars[5].setPosition('1595,1000,0')
-    # cars[6].setPosition('2103,1000,0')
-    # cars[7].setPosition('2105,1000,0')
-    # cars[8].setPosition('2095,1000,0')
-    cars[9].setPosition('2090,1000,0')
-
-    cars[10].setPosition('1093,1000,0')
-    cars[11].setPosition('1090,1000,0')
-    cars[12].setPosition('1085,1000,0')
-    cars[13].setPosition('1593,1000,0')
-    cars[14].setPosition('1590,1000,0')
-    cars[15].setPosition('1585,1000,0')
-    cars[16].setPosition('2093,1000,0')
-    cars[17].setPosition('2090,1000,0')
-    cars[18].setPosition('2085,1000,0')
-    cars[19].setPosition('2101,1000,0')
-
-    cars[20].setPosition('1113,1000,0')
-    cars[21].setPosition('1115,1000,0')
-    cars[22].setPosition('1107,1000,0')
-    cars[23].setPosition('1613,1000,0')
-    cars[24].setPosition('1615,1000,0')
-    cars[25].setPosition('1620,1000,0')
-    cars[26].setPosition('2113,1000,0')
-    cars[27].setPosition('2115,1000,0')
-    cars[28].setPosition('2120,1000,0')
-    cars[29].setPosition('2125,1000,0')
-
-
+    
     print("*** Starting network")
     net.build()
     c1.start()
@@ -214,7 +159,153 @@ def topology():
 
     os.system('ovs-ofctl del-flows sw1 -O Openflow13; ovs-ofctl add-flow sw1 "table=0, priority=0, actions=goto_table:1" -O Openflow13; ovs-ofctl del-flows sw2 -O Openflow13; ovs-ofctl add-flow sw2 "table=0, priority=0, actions=goto_table:1" -O Openflow13; ovs-ofctl add-flow sw2 "table=1, priority=0, actions=CONTROLLER:65535" -O Openflow13; ovs-ofctl add-flow sw2 "table=1, priority=0, in_port=1 actions=4" -O Openflow13; ovs-ofctl add-flow sw2 "table=1, priority=0, in_port=2 actions=4" -O Openflow13; ovs-ofctl add-flow sw2 "table=1, priority=0, in_port=3 actions=4" -O Openflow13; ovs-ofctl del-flows rsu1 -O Openflow13; ovs-ofctl del-flows rsu2 -O Openflow13; ovs-ofctl del-flows rsu3 -O Openflow13; ovs-ofctl add-flow rsu1 "table=0, priority=0, actions=goto_table:1" -O Openflow13; ovs-ofctl add-flow rsu2 "table=0, priority=0, actions=goto_table:1" -O Openflow13; ovs-ofctl add-flow rsu3 "table=0, priority=0, actions=goto_table:1" -O Openflow13; ovs-ofctl add-flow rsu1 "table=1, priority=0, in_port=1, actions=3" -O Openflow13; ovs-ofctl add-flow rsu1 "table=1, priority=0, in_port=3, actions=1" -O Openflow13; ovs-ofctl add-flow rsu2 "table=1, priority=0, in_port=1, actions=4" -O Openflow13; ovs-ofctl add-flow rsu2 "table=1, priority=0, in_port=4, actions=1" -O Openflow13; ovs-ofctl add-flow rsu3 "table=1, priority=0, in_port=1, actions=3" -O Openflow13; ovs-ofctl add-flow rsu3 "table=1, priority=0, in_port=3, actions=1" -O Openflow13; ovs-ofctl add-flow rsu3 "table=1, priority=1, cookie=0x28, in_port=1, nw_dst=200.0.10.2,icmp actions=5" -O Openflow13; ovs-ofctl add-flow rsu3 "table=1, priority=1, cookie=0x28,in_port=5, actions=1" -O Openflow13; ovs-ofctl add-flow rsu3 "table=1, priority=1, cookie=0x28, in_port=1, arp actions=3,5" -O Openflow13; ovs-ofctl add-flow rsu2 "table=1, priority=1, cookie=0x28, in_port=1, nw_dst=200.0.10.2,icmp actions=5" -O Openflow13; ovs-ofctl add-flow rsu2 "table=1, priority=1, cookie=0x28,in_port=5, actions=1" -O Openflow13; ovs-ofctl add-flow rsu2 "table=1, priority=1, cookie=0x28, in_port=1, arp actions=4,5" -O Openflow13; ovs-ofctl add-flow rsu1 "table=1, priority=1, cookie=0x28, in_port=1, nw_dst=200.0.10.2,icmp actions=5" -O Openflow13; ovs-ofctl add-flow rsu1 "table=1, priority=1, cookie=0x28,in_port=5, actions=1" -O Openflow13; ovs-ofctl add-flow rsu1 "table=1, priority=1, cookie=0x28, in_port=1, arp actions=3,5" -O Openflow13; ovs-ofctl add-flow rsu1 "table=1, priority=1, cookie=0x28, in_port=1, nw_dst=200.0.10.2,udp,tp_dst=5002 actions=5" -O Openflow13; ovs-ofctl add-flow rsu2 "table=1, priority=1, cookie=0x28, in_port=1, nw_dst=200.0.10.2,udp,tp_dst=5002 actions=5" -O Openflow13; ovs-ofctl add-flow rsu3 "table=1, priority=1, cookie=0x28, in_port=1, nw_dst=200.0.10.2,udp,tp_dst=5002 actions=5" -O Openflow13; mysql -u root -pwifi -e "delete from redirect;" framework 2> /dev/null')
 
-    time.sleep(1)
+    time.sleep(2)
+
+#     os.system('mysql -u root -pwifi < ./framework_its_sdn/initialdb.sql -fv &')
+#     os.system('./framework_its_sdn/lc_mob.sh > j2.txt &')
+#     time.sleep(1)
+#     os.system('./framework_its_sdn/central_controller2.sh > j1.txt &')
+#     os.system('./framework_its_sdn/local_controllers.sh > j3.txt &')
+
+    # # time.sleep(1)
+
+    server_s1.cmd('tcpdump udp port 5002 -i server_s1-eth0 --direction=in -tttttnnvS --immediate-mode -l > server_s1.txt &')
+    server_s2.cmd('tcpdump udp port 5002 -i server_s2-eth0 --direction=in -tttttnnvS --immediate-mode -l > server_s2.txt &')
+    server_s3.cmd('tcpdump udp port 5002 -i server_s3-eth0 --direction=in -tttttnnvS --immediate-mode -l > server_s3.txt &')
+    server_e.cmd('tcpdump udp port 5003 -i server_e-eth0 --direction=in -tttttnnvS --immediate-mode -l > server_e.txt &')
+    server_e2.cmd('tcpdump udp port 5004 -i server_e2-eth0 --direction=in -tttttnnvS --immediate-mode -l > server_e2.txt &')
+    server_g.cmd('tcpdump udp port 5005 -i server_g-eth0 --direction=in -tttttnnvS --immediate-mode -l > server_g.txt &')
+
+
+    print( "*** Iniciando geracao de trafego" )
+
+    # time.sleep(2)
+
+    # for x in xrange(0,40):
+
+    #     cars[x].cmd('tcpdump -i car%d-wlan0 --direction=out -tttttnnvS --immediate-mode -l > car%d.txt &' % (x, x))
+
+    #     print("*** Car[%d] connect to server_s at 1Mbps" %x)
+    #     cars[x].cmdPrint("timeout 205 hping3 --udp -p 5002 -i u10200 -d 1470 200.0.10.2 -q &")
+    #     cars[x].cmdPrint("ping 200.0.10.2 -i 1 -c 205  > ping%d_ss.txt &" %x)
+        
+    #     print("*** Car[%d] connect to server_e at 1Mbps" %x)
+    #     cars[x].cmdPrint("timeout 205 hping3 --udp -p 5003 -i u10200 -d 1470 200.0.10.3 -q &")
+    #     cars[x].cmdPrint("ping 200.0.10.3 -i 1 -c 205  > ping%d_se.txt &" %x)
+
+    #     print("*** Car[%d] connect to server_e2 at 2Mbps" %x)
+    #     cars[x].cmdPrint("timeout 205 hping3 --udp -p 5004 -i u6800 -d 1470 200.0.10.4 -q &")
+    #     cars[x].cmdPrint("ping 200.0.10.4 -i 1 -c 205  > ping%d_se2.txt &" %x)
+
+    #     print("*** Car[%d] connect to server_g at 1Mbps" %x)
+    #     cars[x].cmdPrint("timeout 205 hping3 --udp -p 5005 -i u10200 -d 1470 200.0.10.5 -q &")
+    #     cars[x].cmdPrint("ping 200.0.10.4 -i 1 -c 205  > ping%d_sg.txt &" %x)   
+
+
+#     net.startMobility( time=0 )
+#     net.mobility( cars[0], 'start', time=1, position='120,1000,0' )
+#     net.mobility( cars[0], 'stop', time=179, position='2101,1000,0' )
+#     net.mobility( cars[1], 'start', time=1, position='130,999,0' )
+#     net.mobility( cars[1], 'stop', time=179, position='2095,999,0' )
+#     net.mobility( cars[2], 'start', time=1, position='135,997,0' )
+#     net.mobility( cars[2], 'stop', time=59, position='2097,997,0' )
+#     net.mobility( cars[3], 'start', time=1, position='140,1000,0' )
+#     net.mobility( cars[3], 'stop', time=59, position='1601,1000,0' )
+#     net.mobility( cars[4], 'start', time=1, position='145,999,0' )
+#     net.mobility( cars[4], 'stop', time=59, position='1599,999,0' )
+#     net.mobility( cars[5], 'start', time=1, position='150,997,0' )
+#     net.mobility( cars[5], 'stop', time=179, position='1595,997,0' )
+#     net.mobility( cars[6], 'start', time=1, position='155,1000,0' )
+#     net.mobility( cars[6], 'stop', time=179, position='1101,1000,0' )
+#     net.mobility( cars[7], 'start', time=1, position='160,1000,0' )
+#     net.mobility( cars[7], 'stop', time=179, position='1098,1000,0' )
+#     net.mobility( cars[8], 'start', time=1, position='165,999,0' )
+#     net.mobility( cars[8], 'stop', time=179, position='1095,999,0' )
+#     net.stopMobility( time=180 )
+
+    # RSU3
+    cars[0].setPosition('2115,993,0')
+    cars[1].setPosition('2112,993,0')
+    cars[2].setPosition('2115,995,0')
+    cars[3].setPosition('2112,995,0')
+    cars[4].setPosition('2115,997,0')
+    cars[5].setPosition('2112,997,0')
+    cars[6].setPosition('2109,997,0')
+    
+    # RSU 2
+    cars[7].setPosition('1615,993,0')
+    cars[8].setPosition('1612,993,0')
+    cars[9].setPosition('1615,995,0')
+    cars[10].setPosition('1612,995,0')
+    cars[11].setPosition('1609,995,0')
+    cars[12].setPosition('1606,995,0')
+
+    # RSU 1
+    cars[13].setPosition('2115,993,0')
+    cars[14].setPosition('2112,993,0')
+    cars[15].setPosition('2115,995,0')
+    cars[16].setPosition('2112,995,0')
+    cars[17].setPosition('2115,997,0')
+    cars[18].setPosition('2112,997,0')
+    cars[19].setPosition('2109,997,0')
+
+    cars[20].setPosition('10,993,0')
+    cars[21].setPosition('15,993,0')
+    cars[22].setPosition('20,993,0')
+    cars[23].setPosition('25,993,0')
+    cars[24].setPosition('10,995,0')
+    cars[25].setPosition('15,995,0')
+    cars[26].setPosition('20,995,0')
+    cars[27].setPosition('25,995,0')
+    cars[28].setPosition('10,997,0')
+    cars[29].setPosition('15,997,0')
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    # time.sleep(33)
+
+    # print("Moving Car0 to RSU2")
+    # cars[0].setPosition('1605,975,0')
+
+    # time.sleep(33)
+
+    # print("Moving Car0 to RSU3")
+    # cars[0].setPosition('2105,975,0')
+
+    # time.sleep(33)
+
+    # print("Moving Car1 to RSU2")
+    # cars[1].setPosition('1605,975,0')
+
+    # time.sleep(33)
+
+    # print("Moving Car2 to RSU3")
+    # cars[2].setPosition('2105,975,0')
+
+    # time.sleep(33)
+
+    # print("Moving Car1 to RSU3")
+    # cars[1].setPosition('2105,975,0')
+
+    # time.sleep(33)
+
+    # os.system('fuser -k ./framework_its_sdn/central_controller2.sh')
+    # os.system('fuser -k ./framework_its_sdn/lc_mob.sh')  
+    # os.system('fuser -k ./framework_its_sdn/local_controllers.sh')
+
+
+    # os.system('pkill tcpdump')
+    # os.system('pkill hping3')
+    # os.system('pkill ping')
 
     info("*** Running CLI\n")
     CLI_wifi(net)
