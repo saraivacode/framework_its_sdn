ef2221f0af99387ab3a0d11b959f7993f175964c
saraivacode <tiagosarai@gmail.com>
2019-02-02 15:35:50 -0200

    Update central_controller2.sh

--- central_controller2.sh
+++ central_controller2.sh
@@ -1,32 +1,32 @@
 #!/bin/bash
 
-#Define e aplica filas nos switches e respectivas portas
-echo -e "\n Configurando filas"
+##Set and apply queues on switches
+echo -e "\n Configuring queues"
 
-#zera arquyivo para armazenamento de informações locais
+#erase the file for local information storage
 rm -f temp2.txt
 
-#Coleta os DPIDs dos switchs nas regiões de interesse com respectivas interfaces para aplicar filas e salva em temp2.txt
+#collects the DPIDs of the switches in the regions of interest with their interfaces to apply queues and saves in temp2.txt
 mysql -u root -pwifi -e "select dpid,iface_updtip from rsu where Region IN (select region from appkpi)" framework 2> /dev/null | grep '-' |
 while read i;
 do
 	echo $i >> temp2.txt
 done
 
-#variavel de controle
+#clear the ontrol variable
 t2=0
 
-#Identifica DPIDs
+#Identify DPIDs
 b2=$(cat temp2.txt | awk -F' ' '{print $1}')
 
 
-#Para cada DPID
+#For each DPID
 for i in $b2;
 do
-	#incrementa variavel de controle
+	#inc control variable
 	t2=$(echo $t2+1 | bc)
 
-	#salva emn x2 os valores de DPID e em y2 os valores de inteferface, para montar as expressões REST
+	#saves the values of DPID in x2 and in y2 the values of inteferface, to mount the expressions REST
 	x2=$(cat temp2.txt | awk -F' ' '{print $1}' | cut -d$'\n' -f$t2)
 	y2=$(cat temp2.txt | awk -F' ' '{print $2}' | cut -d$'\n' -f$t2)
 
@@ -46,63 +46,63 @@ do
 
 	#x=$(mysql -u root -pwifi -e "select data_rate from appkpi;" framework 2> /dev/null | grep 0 | while read i;	do echo $i;	done)
 
-	#Quantidade de filas = quantidade de aplicações (contador)
+	#Number of queues = number of applications (counter)
 	c=$(echo $x | wc -w)
 
-	# define as taxas minimas a serem aplicadas para cada fila. Ex {"min_rate": "500000"}
+	# sets the minimum rates to apply for each queue. Ex {"min_rate": "500000"}
 	h=$(for i in $x; do echo {\"min_rate\": "\"$(echo $i)\""},; done | paste -s | cut -d',' -f1-$c)
 
-	#define comando final com max_rate fixos (melhorar/ajustar)
+	#define final command with fixed max_rate (improve / adjust)
 	j=$(echo curl -X POST -d "'{\"port_name\": \"$y2\", \"type\": \"linux-htb\", \"max_rate\": \"5000000\", \"queues\": [{\"max_rate\": \"5000000\"}, $(echo $h)]}'" http://localhost:8080/qos/queue/000000000000000$x2)
 	
-	#Envia comando para o controlador, contendo as filas e as referencias de DPID (switch) e porta
+	#Send command to the controller, containing the queues and references of DPID (switch) and port
 	# echo $j
 	/bin/sh -c "$j"
 
 done
 
-echo -e "\n Configurando Qos"
+echo -e "\n Configuring QoS"
 
-# zera variavel de controle
+# clear the control variable
 t2=0
 
-#Para cada DPID
+#For each DPID
 for i in $b2;
 do
-	#incrementa variavel de controle
+	#inc the control variable
 	t2=$(echo $t2+1 | bc)
 
-	#salva emn x2 os valores de DPID
+	#saves emn x2 the values of DPID
 	x2=$(cat temp2.txt | awk -F' ' '{print $1}' | cut -d$'\n' -f$t2)
 
-	#zera arquivo para armazenamento de informações locais
+	#Clear the file for storing local information
 	rm -f temp.txt
 
-	#busca na base as informações de endereço IP e porta dos servidores onde estão as aplicações e salva localmente em temp.txt
+	#search the IP address and port information of the servers where the applications are located, and save locally in temp.txt
 	mysql -u root -pwifi -e "select infrastructure.ip, appkpi.port from appkpi inner join infrastructure on appkpi.id = infrastructure.app;" framework 2> /dev/null | grep '\.' |
 	while read i;
 	do
 		echo $i >> temp.txt
 	done
 
-	#variavel de controle interna
+	#internal control variable
 	t=0
 
-	# Armazena em b os valores de endereços IP
+	# Stores the values of IP addresses in b
 	b=$(cat temp.txt | awk -F' ' '{print $1}')
 
 
-	#Para cada endereço Ip de servidor de aplicação
+	#For each application server IP address
 	for i in $b;
 	do
-		#incrementa variável de controle
+		#inc control variable
 		t=$(echo $t+1 | bc)
 
-		#Armazena o endereço IP em x e a porta em y
+		#Stores the IP address at x and port on y
 		x=$(cat temp.txt | awk -F' ' '{print $1}' | cut -d$'\n' -f$t)
 		y=$(cat temp.txt | awk -F' ' '{print $2}' | cut -d$'\n' -f$t)
 
-		#Monta e envia o comando associando o tráfego referente ao IP do servidor e a porta, à respectiva fila e em todos os DPIDs
+		#Assembles and sends the command associating the traffic referring to the IP of the server and the port, its queue and in all the DPIDs
 		#echo curl -X POST -d '{"match": {"nw_dst": "'$x'", "nw_proto": "UDP", "tp_dst": "'$y'"}, "actions":{"queue": "'$t'"}}' http://localhost:8080/qos/rules/000000000000000$x2
 		curl -X POST -d '{"match": {"nw_dst": "'$x'", "nw_proto": "UDP", "tp_dst": "'$y'"}, "actions":{"queue": "'$t'"}}' http://localhost:8080/qos/rules/000000000000000$x2
 		curl -X POST -d '{"match": {"nw_dst": "'$x'", "nw_proto": "ICMP"}, "actions":{"queue": "'$t'"}}' http://localhost:8080/qos/rules/000000000000000$x2
@@ -110,4 +110,4 @@ do
 done
 
 
-echo -e "\n finalizado"
+echo -e "\n Finishing"
